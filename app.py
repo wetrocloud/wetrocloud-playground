8a53cecede644b50de776adf11e2e7bb30385cd3
Aluko Michael Boluwatife <alukoboluwatife3@gmail.com>
2025-03-18 10:20:07 -0700

    deepseek reasoning

--- app.py
+++ app.py
@@ -13,36 +13,37 @@ client = AsyncOpenAI(base_url=config('OPENAI_BASE_URL'))
 
 
 MODEL_LISTS = [
-    "llama-3.3-70b","gpt-4o","gpt-4.5-preview","claude-3-7-sonnet-20250219","deepseek-r1-distill-qwen-32b","qwen-2.5-32b","mixtral-8x7b-32768"
+    "llama-3.3-70b","gpt-4o","gpt-4.5-preview","claude-3-7-sonnet-20250219","deepseek-r1-distill-qwen-32b","qwen-2.5-32b","mixtral-8x7b-32768",
     "deepseek-r1-distill-llama-70b","claude-3-5-sonnet-20240620","gpt-4","gpt-3.5-turbo","o3-mini","o1-mini","o1","o1-preview","gemma2-9b-it"
 ]
 IMAGE_MODELS = ["gpt-4o"]
+REASON_MODELS = ["deepseek-r1-distill-qwen-32","deepseek-r1-distill-llama-70b"]
 
-@cl.set_starters
-async def set_starters():
-    return [
-        cl.Starter(
-            label="Morning routine ideation",
-            message="Can you help me create a personalized morning routine that would help increase my productivity throughout the day? Start by asking me about my current habits and what activities energize me in the morning.",
-            icon="/public/favicon.svg",
-            ),
+# @cl.set_starters
+# async def set_starters():
+#     return [
+#         cl.Starter(
+#             label="Morning routine ideation",
+#             message="Can you help me create a personalized morning routine that would help increase my productivity throughout the day? Start by asking me about my current habits and what activities energize me in the morning.",
+#             icon="/public/favicon.svg",
+#             ),
 
-        cl.Starter(
-            label="Explain superconductors",
-            message="Explain superconductors like I'm five years old.",
-            icon="/public/favicon.svg",
-            ),
-        cl.Starter(
-            label="Python script for daily email reports",
-            message="Write a script to automate sending daily email reports in Python, and walk me through how I would set it up.",
-            icon="/public/favicon.svg",
-            ),
-        cl.Starter(
-            label="Text inviting friend to wedding",
-            message="Write a text asking a friend to be my plus-one at a wedding next month. I want to keep it super short and casual, and offer an out.",
-            icon="/public/favicon.svg",
-            )
-        ]
+#         cl.Starter(
+#             label="Explain superconductors",
+#             message="Explain superconductors like I'm five years old.",
+#             icon="/public/favicon.svg",
+#             ),
+#         cl.Starter(
+#             label="Python script for daily email reports",
+#             message="Write a script to automate sending daily email reports in Python, and walk me through how I would set it up.",
+#             icon="/public/favicon.svg",
+#             ),
+#         cl.Starter(
+#             label="Text inviting friend to wedding",
+#             message="Write a text asking a friend to be my plus-one at a wedding next month. I want to keep it super short and casual, and offer an out.",
+#             icon="/public/favicon.svg",
+#             )
+#         ]
 
 @cl.on_chat_start
 async def start():
@@ -68,7 +69,7 @@ async def update_settings(settings):
     cl.user_session.set("model", settings["Model"])
 
 async def call_wetrocloud(query: cl.Message,files, model="llama-3.3-70b"):
-
+    reasoning_mode = False
     messages = cl.user_session.get("messages")
     model = cl.user_session.get("model")
     error = False
@@ -94,11 +95,32 @@ async def call_wetrocloud(query: cl.Message,files, model="llama-3.3-70b"):
             stream=True,
         )
 
-        await msg.send()
-        async for part in stream:
-            if part.choices != []:
-                if token := part.choices[0].delta.content or "":
-                    await msg.stream_token(token)
+        if model in REASON_MODELS:
+            async with cl.Step(name=model, type="llm") as step:
+                # step.input = msg.content
+                await msg.send()
+
+                async for part in stream:
+                    if part.choices != []:
+                        if token := part.choices[0].delta.content or "":
+                            if token == "<think>":
+                                reasoning_mode = True
+                            elif token == "</think>":
+                                reasoning_mode = False
+                            else:
+                                pass
+
+                            if reasoning_mode == True:
+                                await step.stream_token(token)
+                            else:
+                                await msg.stream_token(token)
+                    step.end
+        else:
+            await msg.send()
+            async for part in stream:
+                if part.choices != []:
+                    if token := part.choices[0].delta.content or "":
+                        await msg.stream_token(token)
         messages.append({"role": "assistant", "content": msg.content})
     else:
         m = f"Unfortunately `{model}` doesn't support images, try selecting `gpt-4o` in settings"
